60b439b859cd86b4728db145e0d00f69e807c25e
Connor Power <connor@connorpower.com>
2022-07-20 16:05:04 +1200

    Add fzf fuzzy-finder bin and vim plugin

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -2,6 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'junegunn/fzf'
   use 'godlygeek/tabular'
   use 'preservim/vim-markdown'
   use 'chrisbra/csv.vim'
