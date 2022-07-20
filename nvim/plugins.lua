b211802538712265cbb1990e0e602d512d8b0fbc
Connor Power <connor@connorpower.com>
2022-07-20 16:05:33 +1200

    Add rust-lang vim plugin for syntax highlighting & commands

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -2,6 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'rust-lang/rust.vim'
   use 'junegunn/fzf'
   use 'godlygeek/tabular'
   use 'preservim/vim-markdown'
