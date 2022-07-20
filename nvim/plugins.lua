e6e6327e0340d68a68ae899791eb2006832c0da5
Connor Power <connor@connorpower.com>
2022-07-20 17:53:13 +1200

    Configure rust analyzer in nvim

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -1,16 +1,62 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+  ------------------------------------------------------------ nvim, general ---
+
+  -- nvim package management
   use 'wbthomason/packer.nvim'
-  use 'rust-lang/rust.vim'
+
+  -- Completion framework
+  use 'hrsh7th/nvim-cmp'
+
+  -- Snippet completion source for nvim-cmp
+  use 'hrsh7th/cmp-vsnip'
+
+  -- Useful path completions
+  use 'hrsh7th/cmp-path'
+  
+  -- Useful buffer completions
+  use 'hrsh7th/cmp-buffer'
+
+  -------------------------------------------------------------- project org ---
+
+  -- fuzzy finder
   use 'junegunn/fzf'
+
+  ---------------------------------------------------------------------- LSP ---
+ 
+  -- Common LSP tools
+  use 'neovim/nvim-lspconfig'
+
+  -- LSP completion source for nvim-cmp
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  --------------------------------------------------------------------- Rust ---
+
+  -- file detection, syntax highlighting, formatting
+  use 'rust-lang/rust.vim'
+
+  -- Enable more of the features of rust-analyzer, such as inlay hints
+  use 'simrat39/rust-tools.nvim'
+ 
+  ---------------------------------------------------------------------- csv ---
+
   use 'godlygeek/tabular'
-  use 'preservim/vim-markdown'
   use 'chrisbra/csv.vim'
+
+  ------------------------------------------------------------------markdown ---
+
+  -- GitHub markdwon compatible syntax that doesn't confuse deeply
+  -- nested lists with codeblocks.
+  use 'preservim/vim-markdown'
+
+  ------------------------------------------------------------------- colors ---
+
   use({
     'catppuccin/nvim',
     as = 'catppuccin'
   })
+
   end
 )
 
