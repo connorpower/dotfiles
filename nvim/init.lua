46926809d10fbdd1c3b89e9b68bd87967282b6e5
Connor Power <connor@connorpower.com>
2022-07-21 15:23:11 +1200

    Don't accept nvim autocompletions with carriage return

--- nvim/init.lua
+++ nvim/init.lua
@@ -145,10 +145,6 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    })
   },
 
   -- disable in comments
