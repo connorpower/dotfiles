d8d636d16add74f713e01d4472088107ce3fa662
Connor Power <connor@connorpower.com>
2022-07-22 16:27:16 +1200

    Use default settings for rust inlay hints

--- nvim/init.lua
+++ nvim/init.lua
@@ -89,9 +89,7 @@ require('rust-tools').setup({
         autoSetHints = true,
         hover_with_actions = true,
         inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
+            show_parameter_hints = true,
         },
     },
 })
