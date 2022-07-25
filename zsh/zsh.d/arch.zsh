93d19db494b7aaa0eba147a05c2e8bddf0444cc9
Connor Power <connor@connorpower.com>
2022-07-26 08:07:40 +1200

    Add aliases for hibernation & suspend

--- zsh/zsh.d/arch.zsh
+++ zsh/zsh.d/arch.zsh
@@ -3,3 +3,15 @@ source "${HOME}/.config/zsh.d/linux.zsh"
 alias kboff='light -S 0 -s sysfs/leds/tpacpi::kbd_backlight'
 alias kbon='light -S 50 -s sysfs/leds/tpacpi::kbd_backlight'
 
+function suspend-then-hibernate() {
+    swaylock -f
+    sleep 2
+    sudo systemctl suspend-then-hibernate
+}
+
+function hibernate() {
+    swaylock -f
+    sleep 2
+    sudo systemctl hibernate
+}
+
