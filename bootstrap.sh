3dcfd54d2910746e7b0ab2f8c91eca98e01f466b
Connor Power <connor@connorpower.com>
2022-07-29 22:29:33 +1200

    Add rofi launcher for arch

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,6 +47,8 @@ declare -a FILES_ARCH=(
   "${HOME}/.config/waybar/config                  -> waybar/config"
   "${HOME}/.config/waybar/style.css               -> waybar/style.css"
   "${HOME}/.config/swaylock/config                -> swaylock/config"
+  "${HOME}/.config/rofi/config.rasi               -> rofi/config.rasi"
+  "${HOME}/.config/rofi/catppuccin-mocha.rasi     -> rofi/catppuccin-mocha.rasi"
   "${HOME}/.config/systemd/user/dropbox.service   -> systemd/dropbox.service"
   "${HOME}/.config/systemd/user/ssh-agent.service -> systemd/ssh-agent.service"
 )
