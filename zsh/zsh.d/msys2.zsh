8d71c1cdb23484b3ec13b5919766a972fb720a62
Connor Power <connor@connorpower.com>
2022-10-28 02:56:11 +1300

    Enable Windows native ssh-agent for key storage

--- zsh/zsh.d/msys2.zsh
+++ zsh/zsh.d/msys2.zsh
@@ -1,2 +1,6 @@
 # Named directory for directx project
 export dx="${SCM_DIR}/personal/directx"
+
+# Re-export the Windows Optional Component SSH ahead of everything else so
+# the system's ssh-agent is available for key storage.
+export PATH="/c/Windows/System32/OpenSSH:${PATH}"
