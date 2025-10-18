dcb46827ef95e19ef323ffcc25a1f994586ea67c
Connor Power <connor@connorpower.com>
2025-10-18 14:35:04 -0500

    Add convenience for adding SSH & GPG keys in Debian/WSL2

--- zsh/zsh.d/debian-wsl2.zsh
+++ zsh/zsh.d/debian-wsl2.zsh
@@ -2,5 +2,13 @@
 alias pbcopy='clip.exe'
 alias pbpaste='powershell.exe Get-Clipboard'
 
-# Store SSH keys across terminal sessions
-eval "$(keychain --eval --quiet id_rsa)"
+# Add GPG and SSH keys into persistent agents
+function addkeys() {
+    eval "$(keychain --eval --quiet id_rsa)"
+    eval $(keychain --eval --agents gpg EC2399A673BBCD1F)
+}
+
+function wslcd() {
+    newpath=$(wslpath "${1}")
+    cd "${newpath}"
+}
